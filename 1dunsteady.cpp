041eec1637262ae18f087032e98fa42aaeaaa474
Sr66jith <codemastery101@gmail.com>
2017-12-28 17:14:09 +0530

    Completed Program: Requires correction

--- 1dunsteady.cpp
+++ 1dunsteady.cpp
@@ -7,13 +7,13 @@ using namespace std;
 // Geometry variable
 int g = 0;
 
-float Bi = 2.0, delx = 1.0/node;
+float Bi = 2.0, delx = 1.0/node, delt = 0.5;
 
 int main()
 {
     // Function definitions
     void input(float *, float *, float *, float *);
-    void tdma(float *, float *, float *, float *, float *);
+    void transient(float *, float *, float *, float *, float *);
     void output(float *);
 
     // Variable declarations
@@ -21,7 +21,7 @@ int main()
 
     // Function calls
     input(ld, md, ud, rhs);
-    tdma(ld, md, ud, rhs, temp);
+    transient(ld, md, ud, rhs, temp);
     output(temp);
     return 0;
 }
@@ -29,18 +29,36 @@ int main()
 void input(float ld[], float md[], float ud[], float rhs[])
 {
     int i;
+    float c, a;
 
-    for(i = 1; i < node; i++) {
-        ld[i] = 1.0;
-        md[i] = -2.0;
-        ud[i] = 1.0;
-        rhs[i] = 0.0;
+    c = -delt / (delx * delx);
+    a = 1 - (2 * c);
+
+    for(i = 0; i < node; i++) {
+        ld[i] = c;
+        md[i] = a;
+        ud[i] = c;
+        rhs[i] = 1.0;
     }
 
-    rhs[node-1] = -1;
+    ud[0] = 2 * c;
+    md[node-1] = a + (2 * Bi * delx);
+    ld[node-1] = 2 * c;
 }
 
-void tdma(float ld[], float md[], float rhs[], float x[])
+void transient(float ld[], float md[], float ud[], float rhs[], float temp[])
+{
+    // Function Declarations
+    void tdma(float *, float *, float *, float *, float *);
+    void update(float *, float *);
+
+    for(int i = 0; i <= 1.0; i += delt) {
+        tdma(ld, md, ud, rhs, temp);
+        update(temp, rhs);
+    }
+}
+
+void tdma(float ld[], float md[], float ud[], float rhs[], float x[])
 {
     int k;
 
@@ -49,3 +67,24 @@ void tdma(float ld[], float md[], float rhs[], float x[])
         md[k] = md[k] - (ld[k] * ud[k-1]);
     }
 
+    for(k = 1; k < node; k++) {
+        rhs[k] = rhs[k] - (ld[k] * rhs[k-1]);
+    }
+
+    x[node-1] = rhs[node-1] / md[node-1];
+
+    for(k = node-2; k >=0; k--)
+        x[k] = (rhs[k] - (ud[k] * x[k+1])) / md[k];
+}
+
+void update(float x[], float rhs[])
+{
+    for(int i = 0; i < node; i++)
+        rhs[i] = x[i];
+}
+
+void output(float a[])
+{
+    for(int i = 0; i < node; i++)
+        cout << a[i] << " ";
+}
