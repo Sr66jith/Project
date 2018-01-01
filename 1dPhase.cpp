03091b55eb699110e69ac76c0e60ec29b410d219
Sr66jith <codemastery101@gmail.com>
2018-01-01 15:28:46 +0530

    1 Jan 2018: Added Phase change elements

--- 1dPhase.cpp
+++ 1dPhase.cpp
@@ -1,4 +1,4 @@
-/* 1 D unsteady heat conduction */
+/* 1 D conduction dominated phase change */
 # include <iostream>
 # define node 5
 # define time 1
@@ -7,28 +7,30 @@ using namespace std;
 // Geometry variable
 int g = 0;
 
-float Bi = 2.0, delx = 1.0/(node-1), delt = 0.5;
+float Ste = 0.05, Bi = 2.0, theta_m = 0.2; // Assuming water as the heat transfer fluid
+float delx = 1.0/(node-1), delt = 0.5; 
 
 void output(float *);
 
 int main()
 {
     // Variable declarations
-    float ld[node], md[node], ud[node], rhs[node], temp[node];
+    float ld[node], md[node], ud[node], rhs[node], temp[node], liq_frac[node];
 
     // Function declarations
-    void input(float *, float *, float *, float *);
-    void transient(float *, float *, float *, float *, float *);
+    void input(float *, float *, float *, float *, float *);
+    void transient(float *, float *, float *, float *, float *, float *);
 
     // Function calls
-    input(ld, md, ud, rhs);
-    transient(ld, md, ud, rhs, temp);
+    input(ld, md, ud, rhs, liq_frac);
+    transient(ld, md, ud, rhs, temp, liq_frac);
     output(temp);
 
     return 0;
 }
 
-void input(float ld[], float md[], float ud[], float rhs[])
+// To assign the initial values for the variables
+void input(float ld[], float md[], float ud[], float rhs[], float liq_frac[])
 {
     int i;
     float c, a;
@@ -41,6 +43,7 @@ void input(float ld[], float md[], float ud[], float rhs[])
         md[i] = a;
         ud[i] = c;
         rhs[i] = 1.0;
+        liq_frac[i] = 0.0;
     }
 
     ud[0] = 2 * c;
@@ -48,17 +51,22 @@ void input(float ld[], float md[], float ud[], float rhs[])
     ld[node-1] = 2 * c;
 }
 
-void transient(float ld[], float md[], float ud[], float rhs[], float temp[])
+// To execute the transient function for determining the temp. and liquid
+// fraction
+void transient(float ld[], float md[], float ud[], float rhs[], float temp[], float liq_frac[])
 {
     void tdma(float *, float *, float *, float *, float *);
     void update(float *, float *);
+    void phase(float *, float *);
 
     for(float i = 0; i <= time; i += delt) {
         tdma(ld, md, ud, rhs, temp);
         update(temp, rhs);
+        phase(temp, liq_frac);
     }
 }
 
+// To execute the tdma algorithm
 void tdma(float ld[], float md[], float ud[], float rhs[], float x[])
 {
     int k;
@@ -75,12 +83,26 @@ void tdma(float ld[], float md[], float ud[], float rhs[], float x[])
         x[k] = (rhs[k] - (ud[k] * x[k+1])) / md[k];
 }
 
+// To update the previous temperatures with the new one
 void update(float x[], float rhs[])
 {
     for(int i = 0; i < node; i++)
         rhs[i] = x[i];
 }
 
+// To update the liquid fraction term
+void phase(float temp[], float liq_frac[])
+{
+    int i;
+    float c = (Ste * delt) / (delx * delx);
+
+    for(i = 1; i < (node-1); i++) {
+        if(temp[i] >= theta_m) { // Needs checking
+            liq_frac[i] = liq_frac[i] + 2 * c * theta_m - c * temp[i+1] - c * temp[i-1];
+        }
+
+
+// To output any array of size node
 void output(float a[])
 {
     for(int i = 0; i < node; i++)
