ae4922b9d94bd912dcf268eabf8b7d152344a684
Sr66jith <sreejithnadaraja@gmail.com>
2018-01-29 11:30:51 +0000

    Minor changes

--- 1dPhase.cpp
+++ 1dPhase.cpp
@@ -8,29 +8,31 @@ using namespace std;
 int g = 0;
 
 float Ste = 0.05, Bi = 2.0, theta_m = 1; // Assuming water as the heat transfer fluid
-float delx = 1.0/(node-1), delt = 0.5; 
+float delx = 1.0/(node-1), delt = 0.5;
 
 void output(float *);
 
 int main()
 {
     // Variable declarations
-    float ld[node], md[node], ud[node], rhs[node], theta[node], liq_frac[node];
+    float ld[node], md[node], ud[node], rhs[node];
+    float theta[node], theta_old[node];
+    float liq_frac_old[node], liq_frac[node];
 
     // Function declarations
-    void input(float *, float *, float *, float *, float *);
-    void transient(float *, float *, float *, float *, float *, float *);
+    void input(float *, float *, float *, float *, float *, float *);
+    void transient(float *, float *, float *, float *, float *, float *, float *);
 
     // Function calls
-    input(ld, md, ud, rhs, liq_frac);
-    transient(ld, md, ud, rhs, theta, liq_frac);
+    input(ld, md, ud, rhs, liq_frac, liq_frac_old);
+    transient(ld, md, ud, rhs, theta, liq_frac, theta_old, liq_frac_old);
     output(theta);
 
     return 0;
 }
 
 // To assign the initial values for the variables
-void input(float ld[], float md[], float ud[], float rhs[], float liq_frac[])
+void input(float ld[], float md[], float ud[], float rhs[], float liq_frac[], float liq_frac_old[])
 {
     int i;
     float c, a;
@@ -44,6 +46,7 @@ void input(float ld[], float md[], float ud[], float rhs[], float liq_frac[])
         ud[i] = c;
         rhs[i] = 6.0;
         liq_frac[i] = 0.0;
+        liq_frac_old[i] = 0.0;
     }
 
     ud[0] = 2 * c;
@@ -53,7 +56,7 @@ void input(float ld[], float md[], float ud[], float rhs[], float liq_frac[])
 
 // To execute the transient function for determining the temp. and liquid
 // fraction
-void transient(float ld[], float md[], float ud[], float rhs[], float theta[], float liq_frac[])
+void transient(float ld[], float md[], float ud[], float rhs[], float theta[], float liq_frac[], float theta_old[], float liq_frac_old[])
 {
     void tdma(float *, float *, float *, float *, float *);
     void update(float *, float *);
@@ -61,8 +64,8 @@ void transient(float ld[], float md[], float ud[], float rhs[], float theta[], f
 
     for(float i = 0; i <= time; i += delt) {
         tdma(ld, md, ud, rhs, theta);
-        update(theta, rhs);
-        phase(theta, liq_frac, ld, md, ud);
+        update(theta, rhs, theta_old);
+        phase(theta, liq_frac, ld, md, ud, theta_old, liq_frac_old);
     }
 }
 
@@ -86,12 +89,14 @@ void tdma(float ld[], float md[], float ud[], float rhs[], float x[])
 // To update the previous temperatures with the new one
 void update(float x[], float rhs[])
 {
-    for(int i = 0; i < node; i++)
+    for(int i = 0; i < node; i++) {
+        theta_old[i] = rhs[i];
         rhs[i] = x[i];
+    }
 }
 
 // To update the liquid fraction term
-void phase(float theta[], float liq_frac[], float ld[], float md[], float ud[])
+void phase(float theta[], float liq_frac[], float ld[], float md[], float ud[], float theta_old[], float liq_frac_old[])
 {
     int i;
     float c = (Ste * delt) / (delx * delx);
@@ -99,7 +104,14 @@ void phase(float theta[], float liq_frac[], float ld[], float md[], float ud[])
 
     for(i = 1; i < (node-1); i++) {
 
-        if(theta[i] <= theta_m) { // Needs checking
+        if((theta[i] >= theta_m) && (theta_old[i] < theta_m)) { //Checks start of melting
+            liq_frac[i] = liq_frac_old[i] - (delt * Ste * (theta[i-1] - 2 * theta_m + theta[i+1])) + Ste * (theta_m - theta_old[i]);
+        }
+        else {
+            liq_frac[i] = liq_frac_old[i] + c * (theta[i+1] - (2 * theta_m) + theta[i-1]);
+        }
+
+/*        if(theta[i] <= theta_m) { // Needs checking
             liq_frac[i] = liq_frac[i] - 2 * c * theta_m + c * theta[i+1] + c * theta[i-1];
 
             if((liq_frac[i] < 1) && (liq_frac[i] > 0)) {
@@ -114,6 +126,7 @@ void phase(float theta[], float liq_frac[], float ld[], float md[], float ud[])
                 ud[i] = c1;
             }
         }
+*/
     }
 }
 
